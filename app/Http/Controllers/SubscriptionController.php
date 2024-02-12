542f702fd10911443234b397f875211352235d29
Robert Huamani <robert.huamani@hotmail.com>
2024-02-12 05:50:06 -0500

    finishing subscription registration

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -11,14 +11,25 @@ class SubscriptionController extends Controller
     public function getSubscription(Request $request) 
     {
         try {
-            //if user send info....
-            $data = $request->all();
-
-            //save info subscription
-            
-            return response()->json([
-                'success' => true
+            $data = $request->validate([
+                'amount' => 'required|string',
+                'data' => 'required|json',
+                'order_reference' => 'required|json',
+                'status' => 'required|json',
+                'period' => 'required|json',
+                'payment_intent' => 'required|json',
+                'user_id' => 'nullable|exists:users,id',
+                'package' => 'nullable|exists:packages,id',
+                'transaction_id' => 'nullable|exists:transactions,id',
+                'number' => 'required|integer'
             ]);
+
+            $subscription = Subscription::create($data);
+
+            return response()->json([
+                'message' => 'Subscription created successfully', 
+                'data' => $subscription
+            ], 200);
 
         } catch(Exception $ex) {
             return response()->json(['error' => 'Resource not found'], 404);
